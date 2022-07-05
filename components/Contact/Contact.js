3cc5ab9aef09b32bfd9a1be46605f3a880354144
latz09 <85132328+latz09@users.noreply.github.com>
2022-07-05 13:42:32 -0500

    removed contact form until fix bug

--- components/Contact/Contact.js
+++ components/Contact/Contact.js
@@ -7,20 +7,21 @@ const Contact = () => {
 	return (
 		<>
 			<h1 className='text-red-500 text-center text-lg md:text-2xl mt-12'>Contact me</h1>{' '}
-			<div className='grid gap-4 mt-11 md:grid-cols-2 justify-items-center items-center tracking-wider'>
+		<span className="flex justify-center mt-8">	<Social /></span>
+			{/* <div className='grid gap-4 mt-11 md:grid-cols-2 justify-items-center items-center tracking-wider'> */}
 				{' '}
-				<div>
+				<div className="text-center mt-4">
 					<div className='text-white grid gap-4 text-lg'>
 						<h1>Jordan Latz</h1>
-
 						<span>latz.jordan@gmail.com</span>
 						<p>West St Paul, MN 55118</p>
+				
 					</div>
 				</div>
 				<div className="mb-8">
-					<ContactForm />
+					{/* <ContactForm /> */}
 				</div>
-			</div>
+			{/* </div> */}
 		</>
 	);
 };
