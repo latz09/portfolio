badbd69a8dcfa10c0283c318151d89105d0d9ff3
latz09 <85132328+latz09@users.noreply.github.com>
2022-07-13 12:11:59 -0500

    update resume about

--- components/Resume/MainContent.js
+++ components/Resume/MainContent.js
@@ -7,20 +7,12 @@ const MainContent = () => {
 
 	return (
 		<div className='grid gap-4 bg-gray-800 bg-opacity-60'>
-			<div className='prose text-sm tracking-wider leading-5 text-gray-200 text-justify md:text-right p-5 md:border-l'>
+			<div className='prose text-sm tracking-wider leading-5 text-gray-200 place-self-center p-5 md:border-l'>
 				<p className=''>
-					Elevating my way up in one industry, I have the eagerness to succeed
-					in this next Journey. As a chef I have gained many skills that
-					translate to computer Programming. Such as the willingness to know
-					that learning and acquiring skills is a never-ending journey. I have
-					also gained the art of skillful multitasking and remaining organized
-					to the highest standards. The ability to both work and get along with
-					your team is essential, especially in the chaotic setting of a
-					professional kitchen. An example of a skill that has assisted me in
-					the journey of computer programming is the importance of time
-					management – the ability to plan your work both efficiently and
-					accurately.
+				I am a self-taught programmer who has gathered the necessary tools and knowledge from an intensive bootcamp, deploying apps and the continual absorption of knowledge.  As someone who worked their way from a dishwasher to an Executive Sous Chef I acquiring endless skills and attributes.  
 				</p>
+				<p>I began my Journey learning the “basics” of front-end development and additionally immersed myself into an intensive <span className="font-semibold italic"> Full Stack Web, and Mobile Development Bootcamp. </span> Following my completion of the program I immediately enrolled myself into additional courses continually gathering additional tools and knowledge</p>
+				<p>By developing my own apps from scratch to deployment I have continued the never-ending quest for knowledge and improving my skills </p>
 			</div>
 			<div className='text-gray-200'>
 				<h1 className='text-center text-2xl text-gray-200 mt-4'>EXPERIENCE</h1>
