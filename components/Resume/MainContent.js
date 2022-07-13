c1e32d70e992ea629c58c6c9115d50adc03ebd2b
latz09 <85132328+latz09@users.noreply.github.com>
2022-07-13 12:15:43 -0500

    typo in resume

--- components/Resume/MainContent.js
+++ components/Resume/MainContent.js
@@ -9,10 +9,10 @@ const MainContent = () => {
 		<div className='grid gap-4 bg-gray-800 bg-opacity-60'>
 			<div className='prose text-sm tracking-wider leading-5 text-gray-200 place-self-center p-5 md:border-l'>
 				<p className=''>
-				I am a self-taught programmer who has gathered the necessary tools and knowledge from an intensive bootcamp, deploying apps and the continual absorption of knowledge.  As someone who worked their way from a dishwasher to an Executive Sous Chef I acquiring endless skills and attributes.  
+				I am a self-taught programmer who has gathered the necessary tools and knowledge from an intensive bootcamp, deploying apps and the continual absorption of knowledge.  As someone who worked their way from a dishwasher to an Executive Sous Chef, acquiring endless skills and attributes.  
 				</p>
-				<p>I began my Journey learning the “basics” of front-end development and additionally immersed myself into an intensive <span className="font-semibold italic"> Full Stack Web, and Mobile Development Bootcamp. </span> Following my completion of the program I immediately enrolled myself into additional courses continually gathering additional tools and knowledge</p>
-				<p>By developing my own apps from scratch to deployment I have continued the never-ending quest for knowledge and improving my skills </p>
+				<p>I began my Journey learning the “basics” of front-end development and additionally immersed myself into an intensive <span className="font-semibold italic"> Full Stack Web, and Mobile Development Bootcamp. </span> Following my completion of the program I immediately enrolled myself into additional courses to continue gathering additional tools and knowledge</p>
+				<p>By developing my own apps, from scratch to deployment, I continue the never-ending quest for knowledge and improving my skills </p>
 			</div>
 			<div className='text-gray-200'>
 				<h1 className='text-center text-2xl text-gray-200 mt-4'>EXPERIENCE</h1>
