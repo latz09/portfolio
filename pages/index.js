1ba6171df3b80af0580430b03f4429c28be72cce
latz09 <85132328+latz09@users.noreply.github.com>
2022-07-13 11:16:30 -0500

    centered home screen for mobile

--- pages/index.js
+++ pages/index.js
@@ -13,12 +13,12 @@ export default function Home() {
 				<title>Jordan Latz - web developer -</title>
 				<meta name='description' content='Find a great programmer here' />
 			</Head>
-			<div className='grid h-screen content-center items-center'>
+			<div className='grid min-h-screen content-center items-center'>
 				<div className='gap-8 grid md:grid-cols-3 items-center justify-items-center text-center '>
 					<div className='md:col-span-2'>
 						<Greeting />
 					</div>
-					<div className='justify-self-center'>
+					<div className='justify-self-center mb-32 sm:mb-0'>
 						<ProjectBtn />
 					</div>
 				</div>
